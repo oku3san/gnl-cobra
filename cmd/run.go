5a24f12f9867c999572ce253fcdd719600153c8c
oku3san <takaya0303@gmail.com>
2023-03-02 23:21:21 +0900

    Update

--- cmd/run.go
+++ cmd/run.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -12,16 +8,10 @@ import (
     "github.com/spf13/cobra"
 )
 
-// runCmd represents the run command
 var runCmd = &cobra.Command{
     Use:   "run",
-    Short: "A brief description of your command",
-    Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+    Short: "Run git add, commit and push",
+    Long:  "Run git add, commit and push",
     Run: func(cmd *cobra.Command, args []string) {
         command := exec.Command("git", "add", "-A")
         command.Stdout = os.Stdout
--- cmd/status.go
+++ cmd/status.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -12,16 +8,10 @@ import (
     "github.com/spf13/cobra"
 )
 
-// statusCmd represents the status command
 var statusCmd = &cobra.Command{
     Use:   "status",
-    Short: "A brief description of your command",
-    Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+    Short: "Run git status command",
+    Long:  "Run git status command",
     Run: func(cmd *cobra.Command, args []string) {
         command := exec.Command("git", "status")
         command.Stdout = os.Stdout
@@ -34,14 +24,4 @@ to quickly create a Cobra application.`,
 
 func init() {
     rootCmd.AddCommand(statusCmd)
-
-    // Here you will define your flags and configuration settings.
-
-    // Cobra supports Persistent Flags which will work for this command
-    // and all subcommands, e.g.:
-    // statusCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-    // Cobra supports local flags which will only run when this command
-    // is called directly, e.g.:
-    // statusCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
